5e500721824d053f26f48ebdba071b1f83f38830
Julius Theodore <autodeterminatio@gmail.com>
2022-09-13 21:50:30 +0300

    settings (email) and permissions added

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,3 +5,12 @@ class SomebodyOrSomething(permissions.BasePermission):
 
     def has_permission(self, request, view):
         pass
+
+
+class AdminOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.is_admin
+        )
