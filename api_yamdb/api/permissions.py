620f65c5f23f288e0c5f94e092a9002b7f566c22
Julius Theodore <autodeterminatio@gmail.com>
2022-09-15 10:29:41 +0300

    add_permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,16 +1,37 @@
-from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class SomebodyOrSomething(permissions.BasePermission):
+class IsAdminOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
-        pass
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user.is_admin
+        )
 
 
-class AdminOnly(permissions.BasePermission):
+class AdminOnly(BasePermission):
 
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated
             and request.user.is_admin
+        )
+
+
+class IsModeratorAuthorOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_moderator
+            or request.user.is_admin
         )
